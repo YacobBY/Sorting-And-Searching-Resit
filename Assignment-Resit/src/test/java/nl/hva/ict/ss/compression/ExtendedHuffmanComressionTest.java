9dfeddfbce32fd436ef501b206adaaf5439af11a
Unknown <yacob101@gmail.com>
2019-02-07 21:15:00 +0100

    Adding extended tests

--- Assignment-Resit/src/test/java/nl/hva/ict/ss/compression/ExtendedHuffmanComressionTest.java
+++ Assignment-Resit/src/test/java/nl/hva/ict/ss/compression/ExtendedHuffmanComressionTest.java
@@ -1,5 +1,18 @@
 package nl.hva.ict.ss.compression;
 
+import org.junit.Before;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 public class ExtendedHuffmanComressionTest extends HuffmanCompressionTest {
-    // Put your tests here...
+    HuffmanCompression compressor;
+
+    @Before
+    public void setup() {
+        compressor = new HuffmanCompression(getClass().getResourceAsStream("/edu/princeton/cs/algs4/Huffman.java"));
+    }
 }
