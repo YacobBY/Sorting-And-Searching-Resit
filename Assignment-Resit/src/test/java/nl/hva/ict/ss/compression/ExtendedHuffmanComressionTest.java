d9099cf0527651cfbf0c854631b2213c7cb91cd7
Unknown <yacob101@gmail.com>
2019-02-07 22:03:41 +0100

    Need to put huffman in node class?

--- Assignment-Resit/src/test/java/nl/hva/ict/ss/compression/ExtendedHuffmanComressionTest.java
+++ Assignment-Resit/src/test/java/nl/hva/ict/ss/compression/ExtendedHuffmanComressionTest.java
@@ -23,15 +23,23 @@ public class ExtendedHuffmanComressionTest extends HuffmanCompressionTest {
         System.out.println(root.getLeft().getCharacter()+ " "+ root.getLeft().getWeight());
         System.out.println(root.getRight().getCharacter()+ " "+ root.getRight().getWeight());
         assertEquals(14, root.getWeight());
-
-        Node left = root.getLeft();
-        Node right = root.getRight();
         assertEquals(3, root.getLeft().getLeft().getWeight());
         assertEquals(4, root.getLeft().getRight().getWeight());
-        assertEquals(7, right.getWeight());
+        assertEquals(7, root.getRight().getWeight());
     }
 
+    @Test //Checks whether it makes sufficient nodes for each leaf and whether middle nodes are empty
+    public void evenLeafsMeansDoubleNodes() {
+        compressor = new HuffmanCompression("aabbccddeeffgghh"); // 3*a, 4*b, 7*c
 
-
+        Node root = compressor.getCompressionTree();
+        System.out.println(root.getLeft().getCharacter()+ " "+ root.getLeft().getWeight());
+        System.out.println(root.getRight().getCharacter()+ " "+ root.getRight().getWeight());
+        assertEquals(16, root.getWeight());
+        assertEquals(2, root.getLeft().getLeft().getLeft().getWeight());
+        assertEquals(2, root.getLeft().getRight().getRight().getWeight());
+        assertEquals(2, root.getRight().getRight().getRight().getWeight());
+        assertEquals("isNull", null, root.getRight().getRight().getCharacter());
+    }
 
 }
