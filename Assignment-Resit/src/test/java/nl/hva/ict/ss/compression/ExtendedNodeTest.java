bbcbcb844ebc7fc675b8bbde1b07ba08a31160b2
Unknown <yacob101@gmail.com>
2019-02-08 11:41:46 +0100

    Making extra node test

--- Assignment-Resit/src/test/java/nl/hva/ict/ss/compression/ExtendedNodeTest.java
+++ Assignment-Resit/src/test/java/nl/hva/ict/ss/compression/ExtendedNodeTest.java
@@ -10,26 +10,45 @@ public class ExtendedNodeTest extends NodeTest {
     // Put your tests here...
     @Test
     public void canReadWhatWasWritten() throws IOException, ClassNotFoundException {
-//        compressor = new HuffmanCompression("aaabbbbccccccc"); // 3*codeListBinaryValueTest, 4*b, 7*c
-//        Node root = compressor.getCompressionTree();
+
+
+        compressor = new HuffmanCompression("aabbc"); // 3*codeListBinaryValueTest, 4*b, 7*c
+        Node root = compressor.getCompressionTree();
+
+
         try (ObjectOutputStream output = new ObjectOutputStream(new FileOutputStream(("huffman-tree.bin")))) {
 
 
             Node tree = new Node(new Node(1, 'b'), new Node(2, 'a'));
-            tree.write(output);
+            root.write(output);
         }
 
         Node tree = null;
         try (ObjectInputStream input = new ObjectInputStream(new FileInputStream(("huffman-tree.bin")))) {
 
-            tree = Node.read(input);
+            root = Node.read(input);
             System.out.println("-----------");
-            System.out.println(tree.getLeft().getLeft().getCharacter());
-            System.out.println(tree.getLeft().getLeft().getWeight());
+
+
         }
 
-        assertEquals(Character.valueOf('b'), tree.getLeft().getCharacter());
-        assertEquals(Character.valueOf('a'), tree.getRight().getCharacter());
+        assertEquals(Character.valueOf('b'), root.getLeft().getCharacter());
+        assertEquals(Character.valueOf('a'), root.getRight().getCharacter());
+    }
+
+    @Test //Checks whether it makes sufficient nodes for each leaf and whether middle nodes are empty
+    public void evenLeafsMeansDoubleNodes() {
+        compressor = new HuffmanCompression("aabbccddeeffgghh"); // 3*codeListBinaryValueTest, 4*b, 7*c
+
+
+        Node root = compressor.getCompressionTree();
+        System.out.println(root.getLeft().getCharacter() + " " + root.getLeft().getWeight());
+        System.out.println(root.getRight().getCharacter() + " " + root.getRight().getWeight());
+        assertEquals(16, root.getWeight());
+        assertEquals(2, root.getLeft().getLeft().getLeft().getWeight());
+        assertEquals(2, root.getLeft().getRight().getRight().getWeight());
+        assertEquals(2, root.getRight().getRight().getRight().getWeight());
+        assertEquals("isNull", null, root.getRight().getRight().getCharacter());
     }
 
 }
